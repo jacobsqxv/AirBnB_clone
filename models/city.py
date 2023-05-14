e573026ac4655c377395f0b5d9361733ad005f03
jacobsqxv <agyeijacobs@outlook.com>
2023-05-14 07:47:58 -0700

    models

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Holds class City
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Representation of city """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""holds class User"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Representation of user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
